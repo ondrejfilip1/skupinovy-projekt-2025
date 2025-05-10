ca184952c334ab0a861a81c4a00fc62d359c3bfd
Tomáš <tomas.tran@student.spsmb.cz>
2025-05-10 12:47:34 +0200

    updating home page - loading screen when opening page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -8,6 +8,7 @@ import React from "react";
 
 export default function Home() {
   const [value, setValue] = useState(0);
+  const [showPage, setShowPage] = useState(false);
   const PERCENTAGE = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
   useEffect(() => {
@@ -17,37 +18,58 @@ export default function Home() {
       index++;
       if (index === PERCENTAGE.length) {
         clearInterval(interval);
+        setTimeout(() => setShowPage(true), 1000);
       }
     }, 300);
     return () => clearInterval(interval);
   }, []);
 
-  const renderLoading = () => (
-    <div className="mx-4">
-      <div className="flex items-center flex-col">
-        <img src={Logo} className="w-[300px]" alt="Logo" />
-      </div>
-      <div className="text-xs text-[0.6rem] my-2">
-        CUSTOM GLITCHES ON UI MAY APPEAR. BASED ON THIS ANALYSIS.
-        <br />
-        DOCUMENT/D/8b547b967cc90266ddc55d7ddc36567f
-        <br />
-        TYPE: CYBERSPACE
-      </div>
-      <div className="border-1 border-white p-1 border_color">
-        <Progress value={value} />
-      </div>
-    </div>
-  );
+  const checkValue = () => {
+    if (value < 100) {
+      return (
+        "LOADING"
+      );
+    } else {
+      return (
+        "DOWNLOADED"
+      );
+    }
+  };
+
+
+  const renderLoading = () => {
+    if (!showPage) {
+        return (
+          <div className="mx-4 flex justify-center flex-col min-h-screen">
+            <div className="flex items-center flex-col">
+              <h2 className="tracking-widest text-3xl font-bold">{checkValue()}</h2>
+            </div>
+            <div className="text-xs text-[0.6rem] my-2">
+              CUSTOM GLITCHES ON UI MAY APPEAR. BASED ON THIS ANALYSIS.
+              <br />
+              DOCUMENT/D/8b547b967cc90266ddc55d7ddc36567f
+              <br />
+              TYPE: CYBERSPACE
+            </div>
+            <div className="border-1 border-white p-1 border_color">
+              <Progress value={value} />
+            </div>
+          </div>
+        );
+    } else {
+      return (
+        <>
+              <Header />
+
+              <Footer />
+        </>
+      );
+    }
+  };
 
   return (
     <>
-      <Header />
-      {value < 100 ? renderLoading() : (
-        <div className="text-center p-4">
-        </div>
-      )}
-      <Footer />
+      {renderLoading()}
     </>
   );
 }
